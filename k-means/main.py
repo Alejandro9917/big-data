ab398e879cee0d90c34fd1fbcade2f7fb14fce27
amelendezmio3 <61640581+Alejandro9917@users.noreply.github.com>
2024-07-09 22:08:54 -0600

    Kmeans

--- k-means/main.py
+++ k-means/main.py
@@ -1,31 +1,51 @@
 from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 import pandas as pd
 import matplotlib.pyplot as pt
-
-x, y = make_blobs(
-    n_samples=100, centers=4, n_features=2, cluster_std=[1, 1.5, 2, 2], random_state=7
-)
-
-print(x)
-
-df_blobs = pd.DataFrame({"x1": x[:, 0], "x2": x[:, 1], "y": y})
+import pathlib
 
 
-def plotClusters2D(x, y, ax):
-    y_uniques = pd.Series(y).unique()
+def plotCluster2D(x, y, ax):
+  y_uniques = pd.Series(y).unique()
+  
+  for i in y_uniques:
+    x[y==i].plot(
+      title = f"{len(y_uniques)} Clusters",
+      kind = "scatter",
+      x = "x1",
+      y = "x2", 
+      marker = f"${i}$",
+      ax = ax
+    )
 
-    for i in y_uniques:
-        x[y == i].plot(
-            title=f"{len(y_uniques)} Clusters",
-            kind="scatter",
-            x="x1",
-            y="x2",
-            marker=f"${i}$",
-            ax=ax,
-        )
+
+base_path = str(pathlib.Path(__file__).parent.resolve()) + "/ecomerce.csv"
+ecommerce_data = pd.read_csv(base_path)
+
+# Clean data
+ecommerce_data.dropna(inplace=True)
+ecommerce_data = ecommerce_data[["price", "review_score", "review_count"]]
 
 
-fig, ax = pt.subplots(1, 1, figsize=(7, 5))
-x, y = df_blobs[["x1", "x2"]], df_blobs["y"]
+# Reduccion de dimensionalidad en variables numericas
+df_numerico = ecommerce_data.select_dtypes(include="number")
 
-plotClusters2D(x, y, ax)
+
+#Reduccion de dimensionalidad
+pca = PCA(n_components=2)
+pca_components = pca.fit_transform(df_numerico)
+
+
+#Conver principals components to Dataframe
+pca_df = pd.DataFrame(pca_components, columns = ["PC1", "PC2"])
+print(pca_df)
+
+
+#Clusterizacion
+kmodel = KMeans(n_clusters=4, random_state=20)
+y_pred = kmodel.fit_predict(pca_df)
+pca_df["Clusters"] = y_pred
+fig, ax = pt.subplots(1,1,figSize=(7,5))
+x, y = pca_df[["PC1", "PC2"]], pca_df["Clusters"]
+plotCluster2D(x,y, ax)
